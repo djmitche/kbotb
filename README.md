7e1e20017d88bebcc4c96fe345edebc287789dcf
Dustin J. Mitchell <dustin@mozilla.com>
2019-11-27 11:55:11 -0500

    updates

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ To change resolution:
 ```
 gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -sOutputFile=output.pdf 000219.pdf
 ```
+
+For rotating, pdfshuffler seems best
+
+Splitting:
+
+
+(note: three-digit page numbers help!)
+
+```
+for p in `seq 1 293`; do gs -sSAFER -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dFirstPage=$p -dLastPage=$p  -sOutputFile=pages/$p.pdf combined-rotated.pdf; done
+```
--- scoresheet-id.py
+++ scoresheet-id.py
@@ -1,45 +1,65 @@
 import os, sys, subprocess, time
 
+
 def pdfs():
-    for dir in os.listdir('.'):
-        if os.path.isdir(dir) and dir[0] != '.':
-            for f in os.listdir(dir):
-                if f[0] != '.':
-                    yield os.path.join(dir, f)
+    for f in os.listdir("pages"):
+        if f[0] != ".":
+            yield os.path.join("pages", f)
+
+
+previous = None
+
 
 def rename(pdf):
+    global previous
     print("processing " + pdf)
-    proc = subprocess.Popen(['gs', '-dBATCH', '-dSAFER', pdf],
-        stdin=subprocess.PIPE, stdout=open('/dev/null'))
-    number = input("number: ")
-    proc.stdin.write(b'\n')
+    proc = subprocess.Popen(
+        ["gs", "-dBATCH", "-dSAFER", pdf],
+        stdin=subprocess.PIPE,
+        stdout=open("/dev/null"),
+    )
+    sys.stdout.write("number: ")
+    number = sys.stdin.readline().strip()
+    proc.stdin.write(b"\n")
     proc.stdin.close()
     if not number:
-        return
-    number = int(number)
+        print("using previous: %s" % previous)
+        if previous:
+            number = previous
+        else:
+            return
+    previous = number
+    number = int(number.strip())
+
     for i in range(1, 100):
         filename = "{:06d}-{}.pdf".format(number, i)
         if not os.path.exists(filename):
             break
+    print("renaming to %s" % filename)
     os.rename(pdf, filename)
+
 
 def view():
     views = []
     while True:
         n = int(input("number: "))
         for proc in views:
-            proc.stdin.write(b'\n')
+            proc.stdin.write(b"\n")
             proc.stdin.close()
         views = []
         n = "{:06d}".format(n)
-        for f in os.listdir('.'):
-            if f.startswith(n) and f.endswith('.pdf'):
-                proc = subprocess.Popen(['gs', '-dBATCH', '-dSAFER', f],
-                    stdin=subprocess.PIPE, stdout=open('/dev/null'))
+        for f in os.listdir("."):
+            if f.startswith(n) and f.endswith(".pdf"):
+                proc = subprocess.Popen(
+                    ["gs", "-dBATCH", "-dSAFER", f],
+                    stdin=subprocess.PIPE,
+                    stdout=open("/dev/null"),
+                )
                 views.append(proc)
 
-view()
+
+# view()
 
 
-#for pdf in pdfs():
-#    rename(pdf)
+for pdf in sorted(pdfs()):
+    rename(pdf)
